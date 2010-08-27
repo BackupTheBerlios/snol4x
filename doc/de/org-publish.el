2b04ea28ac290c8315f89245c57bc45523d01869
riffraff <riffraff@users.berlios.de>
2010-08-27 22:33:10 +0200

    Wrote some project description

---
+++ doc/de/org-publish.el
@@ -0,0 +1,5 @@
+(setq org-publish-project-alist
+      '(("snol4x"
+	 :base-directory "."
+	 :publishing-directory "../html/de"
+	 :style "<link rel=\"stylesheet\" href=\"../snol4x.css\" type=\"text/css\">")))
