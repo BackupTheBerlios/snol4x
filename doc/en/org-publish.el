37ad3a78c451911891783042a33f235ec8250c49
riffraff <riffraff@users.berlios.de>
2010-08-29 16:31:02 +0200

    Even some more doc stuff

--- doc/en/org-publish.el
+++ doc/en/org-publish.el
@@ -1,7 +1,7 @@
 (setq org-publish-project-alist
       '(("snol4x"
 	 :base-directory "."
-	 :publishing-directory "../html/de"
+	 :publishing-directory "../html/en"
 	 :section-numbers nil
 	 :email "riffraff@users.berlios.de"
 	 :style "<link rel=\"stylesheet\" href=\"../snol4x.css\" type=\"text/css\">")))
